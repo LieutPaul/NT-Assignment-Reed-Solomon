3f7b4c0ce9aa4a4fe521a8d1279ba36fbb17d244
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-05-10 19:04:41 +0530

    Tried gmpy

---
+++ check.py
@@ -0,0 +1,12 @@
+# Do pip3 install gmpy2
+
+import gmpy2
+
+from gmpy2 import mpz,mpq,mpfr,mpc
+
+gmpy2.set_context(gmpy2.context())
+
+a = mpz(int(input("Enter number: ")))
+b = mpz(int(input("Enter number: ")))
+
+print(a % b)
